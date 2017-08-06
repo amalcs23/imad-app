eeadcea4a5e581131d27473f2fa050c5063d9d7c
amalcs23 <30766279+amalcs23@users.noreply.github.com>
2017-08-06 12:26:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,7 @@
 console.log('Loaded!');
 var img=document.getElementById("img");
-img.onclick = function(){
-    img.style.marginLeft=100px;
+img.onclick = function () {
+    img.style.marginLeft = "100px";
     
-}
+    
+};
